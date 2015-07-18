203099c8d1dae6508d6898f6be302dac9b01de75
Jacob Vigeveno <vigevenoj@gmail.com>
2015-07-18 15:31:59 -0700

    Send back a GeoJSON location in Portland for testing

--- src/main/java/com/sharkbaitextraordinaire/location/resources/LocationUpdateResource.java
+++ src/main/java/com/sharkbaitextraordinaire/location/resources/LocationUpdateResource.java
@@ -1,22 +1,47 @@
 package com.sharkbaitextraordinaire.location.resources;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Path(value = "/location")
 public class LocationUpdateResource {
 
 	@GET
 	public String getLocation() {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		Map<String,Object> geojson = new HashMap<String,Object>();
+		Map<String,Object> geometry = new HashMap<String,Object>();
+		Map<String,Object> properties = new HashMap<String,Object>();
+		geojson.put("type", "Feature");
+		geometry.put("type", "Point");
+		geometry.put("coordinates", new double[] { 45.523, -122.676 });
+		geojson.put("geometry", geometry);
+		geojson.put("properties", "");
+		
+		try {
+			return mapper.writeValueAsString(geojson);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "beep boop";
 	}
+
 	
 //	Example GeoJSON point
 //	 {
 //	  "type": "Feature",
 //	  "geometry": {
 //	    "type": "Point",
-//	    "coordinates": [125.6, 10.1]
+//	    "coordinates": [45.523, -122.676]
 //	  },
 //	  "properties": {
 //	    "name": "Dinagat Islands"
