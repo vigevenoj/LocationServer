72a65e8e25fbafc91307ea92c27cec1c43ccb48c
Jacob Vigeveno <vigevenoj@gmail.com>
2015-08-15 23:03:05 -0700

    Changed updates ordering: returned oldest to newest now. Added method to get updates since a given timestamp

--- src/main/java/com/sharkbaitextraordinaire/location/db/OwntracksUpdateDAO.java
+++ src/main/java/com/sharkbaitextraordinaire/location/db/OwntracksUpdateDAO.java
@@ -15,11 +15,14 @@ public interface OwntracksUpdateDAO {
 	@SqlUpdate("create table owntracksupdates (_type varchar(10), lat varchar(12), lon varchar(12), acc varchar(8), tst bigint, batt varchar(8) )")
 	void createTableIfNotExists();
 	
-	@SqlQuery("select _type, lat, lon, acc, tst, batt from owntracksupdates order by tst desc")
+	@SqlQuery("select _type, lat, lon, acc, tst, batt from owntracksupdates order by tst asc")
 	List<OwntracksUpdate> findAll();
 	
 	@SqlQuery("select _type, lat, lon, acc, tst, batt from owntracksupdates order by tst desc limit 1")
 	OwntracksUpdate findLatest();
+	
+	@SqlQuery("select _type, lat, lon, acc, tst, batt from owntracksupdates where tst >= :tst")
+	List<OwntracksUpdate> findSinceTimestamp(@Bind("tst") Long tst);
 	
 	@SqlUpdate("insert into owntracksupdates (_type, lat, lon, acc, tst, batt) values (:_type, :lat, :lon, :acc, :tst, :batt)")
 	void insert(@Bind("_type") String _type, @Bind("lat") String lat, @Bind("lon") String lon, @Bind("acc") String acc, @Bind("tst") Long tst, @Bind("batt") String batt);
